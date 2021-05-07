67f9fa065e9e5b3477b080340a35f6a4a1834a2c
healimfx <74346290+healimfx@users.noreply.github.com>
2021-05-07 23:44:02 +0900

    selectMenu 함수 추가

---
+++ manager.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "manager.h"
+
+int selectMenu(){
+    int menu;
+    printf("\n*** Handong Global University ***\n");
+    printf("1. 학생 정보 조회\n");
+    printf("2. 학생 정보 추가\n");
+    printf("3. 학생 정보 수정\n");
+    printf("4. 학생 정보 삭제\n");
+    printf("5. 파일저장\n");
+    printf("6. 학생 이름으로 검색\n");
+    printf("7. 학번으로 검색\n");
+    printf("8. 전공 검색\n");
+    printf("8. 학년 검색\n");
+    printf("8. 기숙사별 팀 선정\n");
+    printf("0. 종료\n\n");
+    printf("=> 원하는 메뉴는? ");
+    scanf("%d", &menu);
+    return menu;
+}
