871384b646afad2b94f38d6673f81dff1b56126a
healimfx <22000770@handong.edu>
2021-05-08 22:57:33 +0900

    store.h

--- manager.h
+++ manager.h
@@ -2,35 +2,32 @@
 #include <string.h>
 
 typedef struct{
-	char name[60]; // 학생 이름
-	int id; // 학번
-	int grade; // 학년
-    char major[60]; // 전공
-    int RC; // RC
-    int team; // 팀 번호
-} Person;
+    char type; // 제품 종류
+	char name[60]; // 제품 이름
+	int price; // 제품 가격
+	int wight; // 제품 무게 
+    int count; // 제품 개수
+} Product;
 
 // SELECT
 int selectMenu(); // 메뉴 선택 
-int selectDataNo(Person *p[], int num); // 번호 선택
+int selectDataNo(Product *p[], int num); // 번호 선택
 
 // CRUD
-int createPerson(Person *p); // 명단 추가하기
-void readPerson(Person *p); // 명단 출력하기 
-int updatePerson(Person *p); // 명단 업데이트하기   
-int deletePerson(Person *p[], int no); // 명단 삭제하기
-void listPerson(Person *p[],int num); // 명단 리스트 불러오기
+int createProduct(Product *p); // 제품 추가하기
+void readProduct(Product *p); // 제품 읽어오기
+int updateProduct(Product *p); // 제품 수정하기   
+int deleteProduct(Product *p); // 제품 삭제하기       
+void listProduct(Product *p,int count); // 제품 리스트 가져오기
 
 // FILE
-void saveData(Person *p[], int num); // 데이타 파일 저장하기 
-int loadData(Person *p[]); // 데이타 읽어오기
-
+void saveData(Product *p, int count); // 데이타 파일 저장하기 
+int loadData(Product *p); // 데이타 읽어오기
 
 // SEARCH
-void searchName(Person *p[], int num); // 이름으로 검색
-void searchid(Person *p[], int num); // 학번으로 검색
-void searchmajor(Person *p[], int num); // 전공으로 검색 
-void searchgrade(Person *p[], int num); // 학년으로 검색 
+void searchName(Product *p[], int num); // 이름으로 검색
+void searchType(Product *p[], int num); // 종류로 검색
+void searchPrice(Product *p[], int num); // 가격으로 검색 
 
-// TEAM
-int makeTeam(Person *p[], int num); // 긱사별 팀 선정
+// MANAGER
+void managerStore(Product *p[],int num); // 재고 관리 
